aab09a95bf57c5f63b8287e8aa14a4f887044315
ym-y-u <166213577+ym-y-u@users.noreply.github.com>
2024-09-01 13:11:31 +0900

    Add files via upload

---
+++ storage/framework/views/d1ae2dfda4de18ab90835246c7b48960e8e7f55c.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm fixed-bottom mt-5">
+     <a class="navbar-brand mx-auto" href="<?php echo e(url('/')); ?>">
+         <img src="<?php echo e(asset('img/logo.jpg')); ?>" class="img-fluid">
+     </a>
+</nav><?php /**PATH C:\xampp\htdocs\laravel-samuraimart-add-sort\resources\views/components/footer.blade.php ENDPATH**/ ?>
